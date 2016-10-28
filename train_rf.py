1ae273b0975328bcd9b992cc1ddfd5b2c2d6315e
cph <stegben.benjamin@gmail.com>
2016-10-28 11:12:49 +0800

    add random forest example

---
+++ train_rf.py
@@ -0,0 +1,40 @@
+import sys
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import roc_auc_score
+
+
+def main():
+    fname = sys.argv[1]
+    df = pd.read_csv(fname, comment='#')
+
+    clf = RandomForestClassifier(
+        criterion='gini',
+        max_depth=10,
+        n_estimators=1000,
+        verbose=2,
+    )
+    train_x = df.drop('in_sf', axis=1).values
+    train_y = df['in_sf'].values
+
+    rs = ShuffleSplit(n_splits=2, test_size=0.2)
+    score = []
+    for tr_idx, te_idx in rs.split(train_x):
+        subtrain_x = train_x[tr_idx, :]
+        subtrain_y = train_y[tr_idx]
+        validation_x = train_x[te_idx, :]
+        validation_y = train_y[te_idx]
+
+        clf.fit(subtrain_x, subtrain_y)
+        pred_class = clf.predict(validation_x)
+        pred_prob = clf.predict_proba(validation_x)
+        print(pred_prob)
+        print(pred_class)
+        print('accuracy: {}'.format(clf.score(validation_x, validation_y)))
+        print('ROCAUC: {}'.format(roc_auc_score(validation_y, pred_prob[:, 1])))
+
+
+if __name__ == '__main__':
+    main()
